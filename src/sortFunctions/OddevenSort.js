7d3417795a4051fc710ec68b8250f6c5689acbf5
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-08-21 13:19:36 +0300

    Fix indentations

--- src/sortFunctions/OddevenSort.js
+++ src/sortFunctions/OddevenSort.js
@@ -1,22 +1,26 @@
 export async function* OddevenSort(array, swap, highlight, marksort){
   let sorted = 0;
+
   while(!sorted) {
     sorted = 1;
+
     for (let i = 0; i < array.length; i+=2) {
       yield await highlight([i, i + 1]);
-		  if (array[i] > array[i+1]){
+      if (array[i] > array[i+1]) {
         yield await highlight([i, i + 1]);
-		    yield await swap(i, i+1);
-		    sorted = 0;
+        yield await swap(i, i+1);
+        sorted = 0;
 		  }
 	  }
-	  for (let i = 1; i < array.length; i+=2) {
+
+    for (let i = 1; i < array.length; i+=2) {
       yield await highlight([i, i + 1]);
-		  if (array[i] > array[i+1]){
+      if (array[i] > array[i+1]){
         yield await highlight([i, i + 1]);
-		    yield await swap(i, i+1);
-		    sorted = 0;
-		  }
-	  }
+        yield await swap(i, i+1);
+        sorted = 0;
+      }
+    }
+
 	}
 }
--- src/sortFunctions/ShellSort.js
+++ src/sortFunctions/ShellSort.js
@@ -1,27 +1,33 @@
 export async function* ShellSort(array, swap, highlight, marksort) {
-	let gap = 1;
-	while(gap * 3 + 1 < array.length){
-		gap  = gap * 3 + 1;
-	}
-	while(gap > 0){
-		for (let i = gap; i < array.length; i++) {
-		  let j = i;
-		  let temp = array[i];
-		  yield await highlight([j, j - gap]);
-		  while (j > 0 && array[j] <= array[j - gap]) {
-			  yield await highlight([j, j - gap]);
-			  yield await swap(j - gap, j);
-			  j-=gap;
-		  }
-			array[j] = temp;
-		  if (gap == 1)
-		  {
-			  marksort(0);
-			  marksort(i);
+  let gap = 1;
+
+  while(gap * 3 + 1 < array.length){
+    gap  = gap * 3 + 1;
+  }
+
+  while(gap > 0){
+
+    for (let i = gap; i < array.length; i++) {
+      let j = i;
+      let temp = array[i];
+      yield await highlight([j, j - gap]);
+      while (j > 0 && array[j] <= array[j - gap]) {
+        yield await highlight([j, j - gap]);
+        yield await swap(j - gap, j);
+        j-=gap;
+      }
+
+      array[j] = temp;
+
+      if (gap == 1)
+    {
+        marksort(0);
+        marksort(i);
         yield;
-		  }
-		  
-		}
-		gap = Math.floor((gap - 1)/3);
-	}
+      }
+
+    }
+
+    gap = Math.floor((gap - 1)/3);
+  }
 }
