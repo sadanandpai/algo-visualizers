73eba43fa0d4bae4bf1798814b0457756b87cbf2
Sadanand Pai <sadanand.pai@coindcx.com>
2023-10-02 10:02:14 +0530

    Dark theme fix

--- src/apps/sorting-visualizer/layouts/main.layout.tsx
+++ src/apps/sorting-visualizer/layouts/main.layout.tsx
@@ -1,49 +1,17 @@
-import { PropsWithChildren, useEffect } from "react";
-import { useAppDispatch, useAppSelector } from "@/store/hooks";
-
 import Controller from "@/apps/sorting-visualizer/components/controller/controller";
+import ModeIcon from "@/apps/sorting-visualizer/components/theme/mode-icon";
 import Navbar from "@/apps/sorting-visualizer/components/navbar/navbar";
-import { Theme } from "@/types/interfaces";
+import { PropsWithChildren } from "react";
 import classes from "./layout.module.scss";
 import { menuItems } from "@/apps/sorting-visualizer/config";
-import moonIcon from "/icons/moon.svg";
-import { setTheme } from "@/store/app.slice";
-import sunIcon from "/icons/sun.svg";
-
-const SunIcon = <img src={sunIcon} alt="dark mode" />;
-const MoonIcon = <img src={moonIcon} alt="dark mode" />;
 
 function MainLayout({ children }: PropsWithChildren) {
-  const dispatch = useAppDispatch();
-  const storeTheme = useAppSelector((state) => state.app.theme);
-  const cssTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
-    ? Theme.DARK
-    : Theme.LIGHT;
-  const currentTheme = storeTheme ?? cssTheme;
-
-  useEffect(() => {
-    dispatch(setTheme(currentTheme));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
-    <div className="">
+    <div>
       <Navbar menuItems={menuItems} />
       <Controller />
-      <main className={classes.main}>
-        {children}
-
-        <button
-          className={classes.iconBtn}
-          onClick={() =>
-            dispatch(
-              setTheme(currentTheme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT)
-            )
-          }
-        >
-          {currentTheme === Theme.LIGHT ? SunIcon : MoonIcon}
-        </button>
-      </main>
+      <main className={classes.main}>{children}</main>
+      <ModeIcon />
     </div>
   );
 }
