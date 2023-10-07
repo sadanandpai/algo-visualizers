2b79eb3f55c7e8436081f0bb852f5ca17fe0a332
Ankit Tyagi <61766976+ankittyagi206@users.noreply.github.com>
2023-10-07 18:14:12 +0530

    chore: optimized bubble sort Algorithm
    If the array is already sorted, Bubble Sort will terminate early and exit the loop without performing additional iterations.

--- src/apps/sorting-visualizer/sorting-algorithms/bubble.ts
+++ src/apps/sorting-visualizer/sorting-algorithms/bubble.ts
@@ -10,14 +10,18 @@ export async function* bubbleSort(array: number[]): SortAsyncGenerator {
   let i, j;
 
   for (i = 0; i < array.length; i++) {
+    let isSwapped = false;
     for (j = 0; j < array.length - i - 1; j++) {
       yield* highlight(j, j + 1);
 
       if (array[j] > array[j + 1]) {
         yield* swap(array, j, j + 1);
+        isSwapped = true;
       }
     }
-
+    if (!isSwapped) {
+      break;
+    }
     yield* sort(j);
   }
 }
