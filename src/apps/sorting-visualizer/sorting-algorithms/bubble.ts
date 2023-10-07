700197d376d5a3ea1f27fac8391c4c6dd3b10e34
Ankit Tyagi <61766976+ankittyagi206@users.noreply.github.com>
2023-10-07 18:39:55 +0530

    Update bubble.ts
    fixed formatting

--- src/apps/sorting-visualizer/sorting-algorithms/bubble.ts
+++ src/apps/sorting-visualizer/sorting-algorithms/bubble.ts
@@ -3,13 +3,17 @@ import {
   sort,
   swap,
 } from '@/apps/sorting-visualizer/helpers/algorithm-helpers';
+
 import { SortAsyncGenerator } from '@/apps/sorting-visualizer/models/types';
+
 export async function* bubbleSort(array: number[]): SortAsyncGenerator {
   let i, j;
+
   for (i = 0; i < array.length; i++) {
     let isSwapped = false; // Flag to track whether any swaps were made in this pass
     for (j = 0; j < array.length - i - 1; j++) {
       yield* highlight(j, j + 1);
+
       if (array[j] > array[j + 1]) {
         yield* swap(array, j, j + 1);
         isSwapped = true; // Set to true if a swap occurred in this iteration
