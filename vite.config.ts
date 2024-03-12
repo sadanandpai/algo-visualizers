3cfd458a9890c6373183af9802e2954e65435bd6
Sadanand Pai <sadypai@gmail.com>
2024-03-12 07:08:11 +0530

    dist to docs movement

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,8 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/algo-visualizers/dist/',
+  base: '/algo-visualizers/',
+  build: {
+    outDir: 'docs',
+  },
 });
