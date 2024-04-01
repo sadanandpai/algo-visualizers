7c41b500f49e10d03fdcbd55bd692953b550dbb8
Karthik Nayak <karthik.nayak@oracle.com>
2024-04-01 09:18:10 +0530

    Add n-queen directory path to config file.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import path from 'path';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +10,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       '@sortViz': path.resolve(__dirname, './src/apps/sorting-visualizer'),
       '@pathFinder': path.resolve(__dirname, './src/apps/path-finder'),
+      '@nQueen': path.resolve(__dirname, './src/apps/n-queen'),
     },
   },
   base: '/algo-visualizers/',
