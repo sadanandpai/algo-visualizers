c66455771e75085f1ae0634f66fd4477431b4bca
Karthik Nayak <karthik.nayak@oracle.com>
2024-04-02 06:18:13 +0530

    Abstract controller into seperate component.

--- src/apps/n-queen/components/board/board.tsx
+++ src/apps/n-queen/components/board/board.tsx
@@ -1,56 +1,11 @@
-import { useState } from 'react';
-import { initChessBoard, nQueen } from '../../algorithms/n-queen';
-import { ChessBoard } from '../../models/types';
+import Controller from '@nQueen/components/controller/controller';
 import styles from './board.module.scss';
-import playIcon from '/icons/play.svg';
-import resetIcon from '/icons/reset.svg';
 
-const Board = () => {
-  const [board, setBoard] = useState<ChessBoard>([[]]);
-  const [queen, setQueen] = useState<number>(0);
-  const [speed, setSpeed] = useState<string | number>();
-
-  const handleSetQueen = (value: string) => {
-    setQueen(parseInt(value));
-    setBoard(initChessBoard(parseInt(value)));
-  };
-  const handleQueenPlacement = () => {
-    setBoard(initChessBoard(queen));
-    nQueen(board, 0);
-  };
-
+function Board() {
+  const queen = 4;
   return (
     <div>
-      <section className={styles.header}>
-        <input
-          className="input-queen"
-          type="number"
-          placeholder="Enter the number of queens."
-          min="1"
-          max="8"
-          value={queen}
-          onChange={(e) => handleSetQueen(e.target.value)}
-        />
-        <img
-          onClick={handleQueenPlacement}
-          src={playIcon}
-          alt="Play"
-          height={24}
-          width={24}
-        />
-        <img src={resetIcon} height={24} width={24} />
-
-        <input
-          type="range"
-          id="speed"
-          data-tooltip="Animation speed"
-          onChange={(e) => setSpeed(e.target.value)}
-          min={1}
-          max={20}
-          value={speed}
-          step={1}
-        />
-      </section>
+      <Controller />
 
       <div className={styles.board}>
         {Array.from(Array(queen)).map((_, rowIndex) => (
@@ -66,6 +21,6 @@ const Board = () => {
       </div>
     </div>
   );
-};
+}
 
 export default Board;
