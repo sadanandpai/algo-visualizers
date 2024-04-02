88c6a8215ca642ecd444116d41041b667860fd12
Karthik Nayak <karthik.nayak@oracle.com>
2024-04-02 06:16:49 +0530

    Add speed configurations.

---
+++ src/apps/n-queen/config.ts
@@ -0,0 +1,15 @@
+import { Speed } from '@nQueen/models/enum';
+
+export const speeds: Map<string, number> = new Map([
+  [Speed.Infinity, 0],
+  [Speed['4x'], 1],
+  [Speed['2x'], 25],
+  [Speed['1x'], 50],
+  [Speed['0.7x'], 100],
+  [Speed['0.5x'], 250],
+]);
+
+export const defaultSpeeds = {
+  mobile: Speed['1x'],
+  desktop: Speed['4x'],
+};
