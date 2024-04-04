6779e4ee1c7a6ec556ac319a973ddd7facc14524
Karthik Nayak <karthik.nayak@oracle.com>
2024-04-04 06:25:14 +0530

    Add controller component.

--- src/apps/n-queen/layouts/main.layout.tsx
+++ src/apps/n-queen/layouts/main.layout.tsx
@@ -1,13 +1,14 @@
 import Navbar from '@/lib/components/navbar/navbar';
 import ThemeIcon from '@/lib/components/theme-icon/theme-icon';
 import Board from '@nQueen/components/board/board';
+import Controller from '@nQueen/components/controller/controller';
 
 function MainLayout() {
   return (
     <>
       <ThemeIcon bottom={10} right={20} />
       <Navbar title="N Queen Visualizer"></Navbar>
-      {/* <Controller /> */}
+      <Controller />
       <main className="text-center">
         <Board />
       </main>
