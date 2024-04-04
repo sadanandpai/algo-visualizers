c05ec7692f4c14a44645f8c6fd6d86dc39adfa05
Karthik Nayak <karthik.nayak@oracle.com>
2024-04-04 06:22:30 +0530

    Remove controller from board component.

--- src/apps/n-queen/components/board/board.tsx
+++ src/apps/n-queen/components/board/board.tsx
@@ -1,27 +1,21 @@
-import Controller from '@nQueen/components/controller/controller';
-import styles from './board.module.scss';
 import { useAppSelector } from '@/host/store/hooks';
+import styles from './board.module.scss';
 
 function Board() {
   const board = useAppSelector((state) => state.nQueen.board);
-
   return (
-    <div>
-      <Controller />
-
-      <div className={styles.board}>
-        {board.map((row, rowIndex) => (
-          <div key={`${rowIndex}`} className="flex text-center">
-            {row.map((value, colIndex) => (
-              <button
-                className="p-5 text-center w-16 h-16 border border-1-black"
-                data-value={value}
-                key={`${rowIndex}-${colIndex}`}
-              ></button>
-            ))}
-          </div>
-        ))}
-      </div>
+    <div className={styles.board}>
+      {board.map((row, rowIndex) => (
+        <div key={`${rowIndex}`} className="flex text-center">
+          {row.map((value, colIndex) => (
+            <button
+              className="p-5 text-center w-16 h-16 border border-1-black"
+              data-value={value}
+              key={`${rowIndex}-${colIndex}`}
+            ></button>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
